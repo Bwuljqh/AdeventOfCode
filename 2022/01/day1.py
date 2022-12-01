138ffb1d29b38fef291a7fe1fe67ef92c9b9862f
Bwuljqh <30834500+Bwuljqh@users.noreply.github.com>
2022-12-01 23:16:12 +0100

    feat: add graph representation

--- 2022/01/day1.py
+++ 2022/01/day1.py
@@ -1,10 +1,16 @@
 from os import path
+import matplotlib.pyplot as plt
+
 ROOTDIR = path.dirname(__file__)
-file = open(path.join(ROOTDIR, 'inputDay1.txt'),'r')
+file = open(path.join(ROOTDIR, 'inputDay1.txt'), 'r')
 
 elf_troup = ''.join(file.readlines())
-numbered_calories = [[int(elf_calorie) if elf_calorie != '' else 0 for elf_calorie in elf.split('\n') ] for elf in elf_troup.split('\n\n')]
+numbered_calories = [[int(elf_calorie) if elf_calorie != '' else 0 for elf_calorie in elf.split('\n')] for elf in elf_troup.split('\n\n')]
 summed_calories = [sum(elf) for elf in numbered_calories]
-summed_calories.sort()
 
+summed_calories.sort()
 print(sum(summed_calories[-3:]))
+
+plt.figure(dpi=300)
+plt.bar(range(len(summed_calories)), summed_calories)
+plt.show()
