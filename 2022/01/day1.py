c1a7c1a5a546949fcc654492eb4d19a59a439815
Bwuljqh <30834500+Bwuljqh@users.noreply.github.com>
2022-12-02 11:34:39 +0100

    fix: remove last line from first day inpy

--- 2022/01/day1.py
+++ 2022/01/day1.py
@@ -5,7 +5,7 @@ ROOTDIR = path.dirname(__file__)
 file = open(path.join(ROOTDIR, 'inputDay1.txt'), 'r')
 
 elf_troup = ''.join(file.readlines())
-numbered_calories = [[int(elf_calorie) if elf_calorie != '' else 0 for elf_calorie in elf.split('\n')] for elf in elf_troup.split('\n\n')]
+numbered_calories = [[int(elf_calorie) for elf_calorie in elf.split('\n')] for elf in elf_troup.split('\n\n')]
 summed_calories = [sum(elf) for elf in numbered_calories]
 
 summed_calories.sort()
