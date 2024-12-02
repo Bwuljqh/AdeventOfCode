d00d6c3afbce05f754acf4e593a606b0269611bf
Bwuljqh <cyprien.etienne@gmail.com>
2024-12-02 18:47:32 +0100

    chore: cleanup

--- 2024/02/day02.py
+++ 2024/02/day02.py
@@ -1,23 +1,12 @@
 
 import copy
 from os import path
-from collections import Counter
 import numpy as np
-import itertools
-
-itertools.permutations
 
 ROOTDIR = path.dirname(__file__)
 file = open(path.join(ROOTDIR, 'input.txt'), 'r')
 
-# for stream in file.readlines():
-#     print(stream.replace('\n', '').split())
-# exit()
-
-
 INPUTS = [np.array((stream.replace('\n', '').split())).astype("int") for stream in file.readlines()]
-
-counter = 0
 
 
 def test_list(result) -> tuple[bool, None | int]:
@@ -29,11 +18,15 @@ def test_list(result) -> tuple[bool, None | int]:
     return True, None
 
 
+counter_part_1 = 0
+counter_part_2 = 0
+
 for data in INPUTS:
     result = data[1:] - data[:-1]
     test = test_list(result)
     if test[0]:
-        counter += 1
+        counter_part_1 += 1
+        counter_part_2 += 1
     else:
         for i in [test[1] - 1, test[1], test[1] + 1]:
             if i > len(data) or i < 0:
@@ -42,6 +35,8 @@ for data in INPUTS:
             data_copy.pop(i)
             data_copy = np.array(data_copy)
             if test_list(data_copy[1:] - data_copy[:-1])[0]:
-                counter += 1
+                counter_part_2 += 1
                 break
-print(counter)
+
+print("part 1", counter_part_1)
+print("part 2", counter_part_2)
